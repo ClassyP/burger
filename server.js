666cd865a4ed4b835ab3557d3bcb3229ff0674e6
classyp <jeanphilip@mecom>
2018-02-15 15:36:02 -0700

    changing server .js to support handler bars and cofiguring out get routes

--- server.js
+++ server.js
@@ -1,34 +1,39 @@
-// *****************************************************************************
-// Server.js - This file is the initial starting point for the Node/Express server.
-//
-// ******************************************************************************
-// *** Dependencies
-// =============================================================
 var express = require("express");
+var exphbs = require("express-handlebars");
+var path = require('path');
 var bodyParser = require("body-parser");
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var sequelize = require('sequelize');
+var db = require('./models');
 
-// Sets up the Express App
-// =============================================================
+// Create an instance of the express app.
 var app = express();
-var PORT = process.env.PORT || 8080;
-
-// Requiring our models for syncing
-var db = require("./models");
-
-// Sets up the Express app to handle data parsing
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-// parse application/json
+// // Specify the port.
+var port = process.env.PORT || '8080';
+//  Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({
+    defaultLayout: "layout",
+    layoutsDir: __dirname + "/views"
+}));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "handlebars");
+// middlewear
 app.use(bodyParser.json());
-
-// Static directory
-app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+// static files
+app.use(express.static('public'))
+    //serve the favicon
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 // Routes
 // =============================================================
-//require("./routes/api-routes.js")(app);
+app.get("/",function(req,res){
+  
 
+});
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 db.sequelize.sync({force:true}).then(function() {
