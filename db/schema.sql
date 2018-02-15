be6799d2778f533ef5234b615a4ab97a9c7633e4
classyp <jeanphilip@mecom>
2018-02-15 10:30:08 -0700

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ USE burger_db;
 CREATE TABLE epicbuger
 (
 	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	sleepy BOOLEAN DEFAULT false,
+	burger_name varchar(255) NOT NULL,
+	divoure BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
