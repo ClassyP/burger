4ed7a0d24b54af4238a3dad11bf412684bf6f541
classyp <jeanphilip@me.com>
2019-05-22 20:30:44 -0700

    server.js looking better added sql db

--- server.js
+++ server.js
@@ -1,41 +1,26 @@
 var express = require("express");
-var exphbs = require("express-handlebars");
-var path = require('path');
 var bodyParser = require("body-parser");
-var sequelize = require('sequelize');
-var db = require('./models');
 
-// Create an instance of the express app.
+var PORT = process.env.PORT || 8000;
 var app = express();
-// // Specify the port.
-var PORT = process.env.PORT || '8080';
-//  Set Handlebars as the default templating engine.
-app.engine("handlebars", exphbs({
-    defaultLayout: "layout",
-    layoutsDir: __dirname + "/views"
-}));
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "handlebars");
-// middlewear
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-// static files
-app.use(express.static('public'))
-    //serve the favicon
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
-// Routes
-// =============================================================
-app.get("/",function(req,res){
-  res.render("burger",{title: "phil"})
+var exphbs = require("express-handlebars");
 
-});
-// Syncing our sequelize models and then starting our Express app
-// =============================================================
-db.sequelize.sync({force:false}).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgersController.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("Listening on port:%s", PORT);
 });
